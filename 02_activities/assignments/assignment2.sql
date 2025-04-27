a6c7203ed8d1bd26e3830ca9bf167bbe0bcac34d
ddumalag <ddumalag@gmail.com>
2025-04-27 10:30:29 -0400

    Update1 assignment2.sql

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -9,7 +9,7 @@ Using the following syntax you create our super cool and not at all needy manage
 
 SELECT 
 product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
-FROM product
+FROM products
 
 But wait! The product table has some bad data (a few NULL values). 
 Find the NULLs and then using COALESCE, replace the NULL with a 
@@ -20,7 +20,15 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT 
+product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
+FROM product
 
+SELECT 
+product_name || ', ' || 
+coalesce(product_size, '')|| ' (' || 
+coalesce(product_qty_type, 'unit')|| ')'
+FROM product
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -32,17 +40,57 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+select 
+market_date
+, customer_id
+,row_number () OVER(PARTITION by customer_id order by market_date) as visit_number
+from customer_purchases
+
+-- dense rank 
+
+select 
+market_date
+, customer_id
+,dense_rank () OVER(PARTITION by customer_id order by market_date) as visit_number
+from customer_purchases
 
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+--outer query
+select 
+market_date
+, customer_id
+, visit_number
+
+FROM 
+(	
+	select 
+	market_date
+	, customer_id
+	,row_number () OVER(PARTITION by customer_id order by market_date DESC) as visit_number
+	from customer_purchases
+) 
+
+where visit_number = 1
 
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
+-- i don't really get this one ****
+
+
+select 
+product_id
+, customer_id
+, market_date
+
+count (*) over (PARTITION by customer_id, product_id) as purchase_count
+
+from customer_purchases
 
 
 -- String manipulations
@@ -57,10 +105,62 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
+-- trimming
+SELECT
+product_name
+, trim (product_name) as product_name_trimmed
+from product 
 
+-- only capturing organic but the remaining is not null 
+-- if it's just the column + number, it is the starting position and will give you what's remaining 
+SELECT
+product_name
+, substr (product_name, 10) 
+from product 
+
+-- instr 
+SELECT
+product_name
+, instr (product_name, '-') as hyphen_placement 
+from product 
+
+
+-- putting it together somehow 
+-- + 1 gets rid of the hyphen 
+-- instr will give you the position of the hyphen, if no hyphen then it's 0 which will be null at the END
+-- substr product_name and 2 (for example) would have us at the start position of 2 and return everything remaining 
+-- but substr + instr and (+1) it says, find the position of the hyphen plus 1 to not include the space afterward and return what's left i.e. organic, jar, bag
+-- if no hyphen then null 
+
+select 
+product_name
+
+,	case
+		when instr (product_name, '-') > 0 then 
+			trim (substr(product_name, instr(product_name, '-') + 1))
+		else null 
+	end as hyphen_check
+
+from product
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
+select product_size
+from product
+where product_size REGEXP '[0-9]'
+
+-- putting it together? 
+select 
+product_name
+, product_size
+,	case
+		when instr (product_name, '-') > 0 then 
+			trim (substr(product_name, instr(product_name, '-') + 1))
+		else null 
+	end as hyphen_check
+
+from product
+where product_size REGEXP '[0-9]'
 
 
 -- UNION
@@ -73,15 +173,57 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
+-- union: combine the results of two or more queries vertically (removes the duplicates)
+-- step 1: daily total sales by market date
+-- step 2: rank total sales, highest and lowest
+-- step 3: union 
 
+drop table if exists temp.total_daily_sales;
+
+create temp table temp.total_daily_sales as 
+select 
+	market_date
+	, sum (cost_to_customer_per_qty * quantity) as total_sales_per_day
+from customer_purchases
+group by market_date;
+
+Select 
+market_date
+, total_sales_per_day
+
+FROM (
+	select 
+		market_date
+		, total_sales_per_day
+		, row_number () over (order by total_sales_per_day DESC) as daily_sales_rank
+	from temp.total_daily_sales
+) as ranked_sales 
+where daily_sales_rank = 1
+
+UNION 
+
+Select 
+market_date
+, total_sales_per_day
+
+FROM (
+	select 
+		market_date
+		, total_sales_per_day
+		, row_number () over (order by total_sales_per_day ASC) as daily_sales_rank
+	from temp.total_daily_sales
+) as ranked_sales 
+where daily_sales_rank = 1
 
 
 /* SECTION 3 */
 
--- Cross Join
+-- FOR DD: NEED TO REVIEW
+-- Cross Join 
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
 customer on record. How much money would each vendor make per product? 
 Show this by vendor_name and product name, rather than using the IDs.
+
 
 HINT: Be sure you select only relevant columns and rows. 
 Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely be a subquery. 
@@ -89,13 +231,70 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+-- "how much money would each vendor make per product?" isn't this just asking what the price of each product is x 5?
+-- return will be 8 rows*
+-- i want vendor_id, vendor_name, product_id, product_name, cost_to_customer_per_qty * 5, 
 
+select DISTINCT 
+vendor_id
+from vendor_inventory
+-- 3 distict vendors 
 
+select DISTINCT 
+product_id
+from vendor_inventory
+-- 8 distinct product ids 
+
+select DISTINCT
+customer_id
+from customer_purchases
+-- 26 total customer ids 
+
+-- hmmmmmm
+SELECT 
+    vi.vendor_id
+    , cp.product_id
+    , cp.cost_to_customer_per_qty * 5 AS cost_times_5
+FROM vendor_inventory vi
+INNER JOIN customer_purchases cp 
+    ON vi.product_id = cp.product_id
+
+	
+-- needed help
+WITH customer_count AS (
+    SELECT COUNT(DISTINCT customer_id) AS num_customers
+    FROM customer_purchases
+),
+
+vendor_product_prices AS (
+    SELECT 
+        vi.vendor_id,
+        vi.product_id,
+        cp.cost_to_customer_per_qty
+    FROM vendor_inventory vi
+    INNER JOIN customer_purchases cp
+        ON vi.vendor_id = cp.vendor_id
+       AND vi.product_id = cp.product_id
+    GROUP BY vi.vendor_id, vi.product_id
+)
+
+SELECT 
+    vpp.vendor_id,
+    p.product_name,
+    vpp.avg_price * 5 * cc.num_customers AS total_revenue
+FROM vendor_product_prices vpp
+INNER JOIN product p
+    ON vpp.product_id = p.product_id
+CROSS JOIN customer_count cc
+	
+	
 -- INSERT
 /*1.  Create a new table "product_units". 
 This table will contain only products where the `product_qty_type = 'unit'`. 
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
+
+
 
 
 
